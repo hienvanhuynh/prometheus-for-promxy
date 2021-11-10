7f3ca7a6430169752b0dc8ffa467c0598abe21a4
Thomas Jackson <jacksontj.89@gmail.com>
2021-11-10 06:46:24 -0800

    Don't drop ParenExpr when creating StepInvariantExpr (#9591)
    * Add test case to showcase the problem in #9590
    
    Signed-off-by: Thomas Jackson <jacksontj.89@gmail.com>
    
    * Don't unwrap ParenExpr in newStepInvariantExpr
    
    Fixes #9590
    
    Signed-off-by: Thomas Jackson <jacksontj.89@gmail.com>

--- promql/engine_test.go
+++ promql/engine_test.go
@@ -1567,9 +1567,10 @@ func TestQueryLogger_error(t *testing.T) {
 func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 	startTime := time.Unix(1000, 0)
 	endTime := time.Unix(9999, 0)
-	var testCases = []struct {
-		input    string      // The input to be parsed.
-		expected parser.Expr // The expected expression AST.
+	testCases := []struct {
+		input      string      // The input to be parsed.
+		expected   parser.Expr // The expected expression AST.
+		outputTest bool
 	}{
 		{
 			input: "123.4567",
@@ -1579,7 +1580,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					PosRange: parser.PositionRange{Start: 0, End: 8},
 				},
 			},
-		}, {
+		},
+		{
 			input: `"foo"`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.StringLiteral{
@@ -1587,7 +1589,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					PosRange: parser.PositionRange{Start: 0, End: 5},
 				},
 			},
-		}, {
+		},
+		{
 			input: "foo * bar",
 			expected: &parser.BinaryExpr{
 				Op: parser.MUL,
@@ -1613,7 +1616,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 				},
 				VectorMatching: &parser.VectorMatching{Card: parser.CardOneToOne},
 			},
-		}, {
+		},
+		{
 			input: "foo * bar @ 10",
 			expected: &parser.BinaryExpr{
 				Op: parser.MUL,
@@ -1642,7 +1646,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 				},
 				VectorMatching: &parser.VectorMatching{Card: parser.CardOneToOne},
 			},
-		}, {
+		},
+		{
 			input: "foo @ 20 * bar @ 10",
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.BinaryExpr{
@@ -1672,7 +1677,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					VectorMatching: &parser.VectorMatching{Card: parser.CardOneToOne},
 				},
 			},
-		}, {
+		},
+		{
 			input: "test[5s]",
 			expected: &parser.MatrixSelector{
 				VectorSelector: &parser.VectorSelector{
@@ -1688,7 +1694,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 				Range:  5 * time.Second,
 				EndPos: 8,
 			},
-		}, {
+		},
+		{
 			input: `test{a="b"}[5y] @ 1603774699`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.MatrixSelector{
@@ -1708,7 +1715,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					EndPos: 28,
 				},
 			},
-		}, {
+		},
+		{
 			input: "sum by (foo)(some_metric)",
 			expected: &parser.AggregateExpr{
 				Op: parser.SUM,
@@ -1728,7 +1736,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					End:   25,
 				},
 			},
-		}, {
+		},
+		{
 			input: "sum by (foo)(some_metric @ 10)",
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.AggregateExpr{
@@ -1751,7 +1760,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					},
 				},
 			},
-		}, {
+		},
+		{
 			input: "sum(some_metric1 @ 10) + sum(some_metric2 @ 20)",
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.BinaryExpr{
@@ -1795,7 +1805,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					},
 				},
 			},
-		}, {
+		},
+		{
 			input: "some_metric and topk(5, rate(some_metric[1m] @ 20))",
 			expected: &parser.BinaryExpr{
 				Op: parser.LAND,
@@ -1853,7 +1864,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					},
 				},
 			},
-		}, {
+		},
+		{
 			input: "time()",
 			expected: &parser.Call{
 				Func: parser.MustGetFunction("time"),
@@ -1863,7 +1875,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					End:   6,
 				},
 			},
-		}, {
+		},
+		{
 			input: `foo{bar="baz"}[10m:6s]`,
 			expected: &parser.SubqueryExpr{
 				Expr: &parser.VectorSelector{
@@ -1881,7 +1894,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 				Step:   6 * time.Second,
 				EndPos: 22,
 			},
-		}, {
+		},
+		{
 			input: `foo{bar="baz"}[10m:6s] @ 10`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.SubqueryExpr{
@@ -1902,7 +1916,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					EndPos:    27,
 				},
 			},
-		}, { // Even though the subquery is step invariant, the inside is also wrapped separately.
+		},
+		{ // Even though the subquery is step invariant, the inside is also wrapped separately.
 			input: `sum(foo{bar="baz"} @ 20)[10m:6s] @ 10`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.SubqueryExpr{
@@ -1933,7 +1948,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					EndPos:    37,
 				},
 			},
-		}, {
+		},
+		{
 			input: `min_over_time(rate(foo{bar="baz"}[2s])[5m:] @ 1603775091)[4m:3s]`,
 			expected: &parser.SubqueryExpr{
 				Expr: &parser.StepInvariantExpr{
@@ -1980,7 +1996,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 				Step:   3 * time.Second,
 				EndPos: 64,
 			},
-		}, {
+		},
+		{
 			input: `some_metric @ 123 offset 1m [10m:5s]`,
 			expected: &parser.SubqueryExpr{
 				Expr: &parser.StepInvariantExpr{
@@ -2001,7 +2018,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 				Step:   5 * time.Second,
 				EndPos: 36,
 			},
-		}, {
+		},
+		{
 			input: `some_metric[10m:5s] offset 1m @ 123`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.SubqueryExpr{
@@ -2022,7 +2040,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					EndPos:         35,
 				},
 			},
-		}, {
+		},
+		{
 			input: `(foo + bar{nm="val"} @ 1234)[5m:] @ 1603775019`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.SubqueryExpr{
@@ -2067,7 +2086,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					EndPos:    46,
 				},
 			},
-		}, {
+		},
+		{
 			input: "abs(abs(metric @ 10))",
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.Call{
@@ -2104,7 +2124,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					},
 				},
 			},
-		}, {
+		},
+		{
 			input: "sum(sum(some_metric1 @ 10) + sum(some_metric2 @ 20))",
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.AggregateExpr{
@@ -2155,7 +2176,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					},
 				},
 			},
-		}, {
+		},
+		{
 			input: `foo @ start()`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.VectorSelector{
@@ -2171,7 +2193,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					StartOrEnd: parser.START,
 				},
 			},
-		}, {
+		},
+		{
 			input: `foo @ end()`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.VectorSelector{
@@ -2187,7 +2210,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					StartOrEnd: parser.END,
 				},
 			},
-		}, {
+		},
+		{
 			input: `test[5y] @ start()`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.MatrixSelector{
@@ -2207,7 +2231,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					EndPos: 18,
 				},
 			},
-		}, {
+		},
+		{
 			input: `test[5y] @ end()`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.MatrixSelector{
@@ -2227,7 +2252,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					EndPos: 16,
 				},
 			},
-		}, {
+		},
+		{
 			input: `some_metric[10m:5s] @ start()`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.SubqueryExpr{
@@ -2248,7 +2274,8 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 					EndPos:     29,
 				},
 			},
-		}, {
+		},
+		{
 			input: `some_metric[10m:5s] @ end()`,
 			expected: &parser.StepInvariantExpr{
 				Expr: &parser.SubqueryExpr{
@@ -2270,6 +2297,61 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 				},
 			},
 		},
+		{
+			input:      `floor(some_metric / (3 * 1024))`,
+			outputTest: true,
+			expected: &parser.Call{
+				Func: &parser.Function{
+					Name:       "floor",
+					ArgTypes:   []parser.ValueType{parser.ValueTypeVector},
+					ReturnType: parser.ValueTypeVector,
+				},
+				Args: parser.Expressions{
+					&parser.BinaryExpr{
+						Op: parser.DIV,
+						LHS: &parser.VectorSelector{
+							Name: "some_metric",
+							LabelMatchers: []*labels.Matcher{
+								parser.MustLabelMatcher(labels.MatchEqual, "__name__", "some_metric"),
+							},
+							PosRange: parser.PositionRange{
+								Start: 6,
+								End:   17,
+							},
+						},
+						RHS: &parser.StepInvariantExpr{
+							Expr: &parser.ParenExpr{
+								Expr: &parser.BinaryExpr{
+									Op: parser.MUL,
+									LHS: &parser.NumberLiteral{
+										Val: 3,
+										PosRange: parser.PositionRange{
+											Start: 21,
+											End:   22,
+										},
+									},
+									RHS: &parser.NumberLiteral{
+										Val: 1024,
+										PosRange: parser.PositionRange{
+											Start: 25,
+											End:   29,
+										},
+									},
+								},
+								PosRange: parser.PositionRange{
+									Start: 20,
+									End:   30,
+								},
+							},
+						},
+					},
+				},
+				PosRange: parser.PositionRange{
+					Start: 0,
+					End:   31,
+				},
+			},
+		},
 	}
 
 	for _, test := range testCases {
@@ -2277,6 +2359,9 @@ func TestPreprocessAndWrapWithStepInvariantExpr(t *testing.T) {
 			expr, err := parser.ParseExpr(test.input)
 			require.NoError(t, err)
 			expr = PreprocessExpr(expr, startTime, endTime)
+			if test.outputTest {
+				require.Equal(t, test.input, expr.String(), "error on input '%s'", test.input)
+			}
 			require.Equal(t, test.expected, expr, "error on input '%s'", test.input)
 		})
 	}
